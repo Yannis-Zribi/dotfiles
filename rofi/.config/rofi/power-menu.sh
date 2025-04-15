48836c38e5ece0cdba72b726d04f0e25c9f9e537
Yannis-Zribi <yannis.zribi@proton.me>
2025-04-15 13:02:21 +0200

    remove old rofi scripts

--- rofi/.config/rofi/power-menu.sh
+++ rofi/.config/rofi/power-menu.sh
@@ -2,13 +2,13 @@
 
 # Power menu script using tofi
 
-CHOSEN=$(printf "🔐 Lock\n🟠 Reboot\n🔴 Shutdown\n🖥️ Duplicate Screen\n" | rofi -dmenu)
+CHOSEN=$(printf "🔐 Lock\n🟠 Reboot\n🔴 Shutdown\n🖥️ Copy Screen\n" | rofi -dmenu)
 
 case "$CHOSEN" in
-"🔐 Lock") i3lock ;;
+"🔐 Lock") i3lock -c 000000 ;;
 "🟠 Reboot") reboot ;;
 "🔴 Shutdown") poweroff ;;
-"🖥️ Duplicate Screen") xrandr --output HDMI-A-0 --mode 1920x1080 --same-as eDP ;;
+"🖥️ Copy Screen") xrandr --output HDMI-A-0 --mode 1920x1080 --same-as eDP ;;
 
 *) exit 1 ;;
 esac
