fde2fd6114f2d768515065416b92716dfc8f03ff
Yannis-Zribi <yannis.zribi@proton.me>
2026-03-03 10:07:04 +0100

    ajout du script d'install

---
+++ scripts/.scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo pacman -Syuu --noconfirm
+sudo pacman -S --noconfirm ly i3 i3lock yay fprintd libfprint alacritty picom polybar obsidian brightnessctl python-pipx docker wireguard-tools rofi rofi-emoji rofi-calc xclip maim zsh
+sudo systemctl enable --now ly@tty1.service
+sudo systemctl enable --now docker
+sudo systemctl enable --now systemd-resolved.service
+yay -S --noconfirm ttf-meslo-nerd google-chrome
+pipx install exegol
+pipx ensurepath
+sudo usermod -aG docker $(id -u -n)
+sudo fprintd-enroll yannis
